eabebc2b30854528bdc0f6adc622a334097c1064
Banzak <leosantanabr@gmail.com>
2025-08-01 21:36:04 -0300

    Iniciando projeto

---
+++ src/main/java/com/ngbilling/devcheckout/DevCheckoutApplication.java
@@ -0,0 +1,13 @@
+package com.ngbilling.devcheckout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevCheckoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevCheckoutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ngbilling/devcheckout/DevCheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngbilling.devcheckout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevCheckoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
