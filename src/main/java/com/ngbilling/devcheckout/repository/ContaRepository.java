393f151a21070b5fd5f318e90080942c7073134d
Banzak <leosantanabr@gmail.com>
2025-08-02 16:17:23 -0300

    Criado metodo para consulta da conta pelo numero.

--- src/main/java/com/ngbilling/devcheckout/repository/ContaRepository.java
+++ src/main/java/com/ngbilling/devcheckout/repository/ContaRepository.java
@@ -2,7 +2,11 @@ package com.ngbilling.devcheckout.repository;
 
 import com.ngbilling.devcheckout.model.Conta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
 
 public interface ContaRepository extends JpaRepository<Conta, Long> {
+    Optional<Conta> findByNumeroConta(Integer numeroConta);
 
 }
