d054039bf2d000f87e50a3147dc19c18be9ede48
Banzak <leosantanabr@gmail.com>
2025-08-02 16:31:56 -0300

    Implementada o id gerado automaticamente para cada conta.

--- src/main/java/com/ngbilling/devcheckout/model/Conta.java
+++ src/main/java/com/ngbilling/devcheckout/model/Conta.java
@@ -1,15 +1,19 @@
 package com.ngbilling.devcheckout.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
 import lombok.Data;
 import java.math.BigDecimal;
 
 @Data
 @Entity
+@Table(name = "conta")
 public class Conta {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
     private Integer numeroConta;
 
     private BigDecimal saldo;
