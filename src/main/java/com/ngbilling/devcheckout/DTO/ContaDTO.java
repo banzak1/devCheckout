412e8e3b5b465c94845a37e7b4a70383e631ebad
Banzak <leosantanabr@gmail.com>
2025-08-02 16:15:32 -0300

    Criado DTO para a Conta

---
+++ src/main/java/com/ngbilling/devcheckout/DTO/ContaDTO.java
@@ -0,0 +1,12 @@
+package com.ngbilling.devcheckout.DTO;
+
+
+import java.math.BigDecimal;
+
+public class ContaDTO {
+    public Integer numeroConta;
+    public BigDecimal saldo;
+
+    public ContaDTO(Integer numeroConta, BigDecimal saldo) {
+    }
+}
