cba3a4ddc1ebc8960dff28801942ada468a7cd24
Banzak <leosantanabr@gmail.com>
2025-08-02 22:26:02 -0300

    Cria Controller da transacao.

---
+++ src/main/java/com/ngbilling/devcheckout/resourse/TransacaoController.java
@@ -0,0 +1,24 @@
+package com.ngbilling.devcheckout.resourse;
+
+import com.ngbilling.devcheckout.DTO.TransacaoDTO;
+import com.ngbilling.devcheckout.model.Conta;
+import com.ngbilling.devcheckout.service.TransacaoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TransacaoController {
+
+    private final TransacaoService transacaoService;
+
+    public TransacaoController(TransacaoService transacaoService) {
+        this.transacaoService = transacaoService;
+    }
+
+    @PostMapping("/transacao")
+    public ResponseEntity<Conta> efetuarTransacao(@RequestBody TransacaoDTO transacaoDTO) {
+        return transacaoService.efetuaPagamento(transacaoDTO);
+    }
+}
